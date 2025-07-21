dda68d7dbfe287dc4f11c86b315fcf72a58f77db
Sahil-Karanth <1paleking1@gmail.com>
2025-07-21 19:54:49 +0100

    added loki keyboard firmware to repo

---
+++ loki_keyboard_firmware/keymap.c
@@ -0,0 +1,66 @@
+#include QMK_KEYBOARD_H
+#include "raw_hid.h"
+
+const hsv_t colour_map[] = {
+    { HSV_RED },     // Layer 0
+    { HSV_BLUE },    // Layer 1
+    { HSV_WHITE },   // Layer 2
+    { HSV_PURPLE },  // Layer 3
+    { HSV_YELLOW },  // Layer 4
+    { HSV_GREEN },   // Layer 5
+    { HSV_CYAN }     // Layer 6
+};
+
+#define COLOUR_MAP_SIZE (sizeof(colour_map) / sizeof(colour_map[0]))
+
+void keyboard_post_init_user(void) {
+    rgblight_enable();
+    rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
+    rgblight_sethsv(HSV_BLUE);
+}
+
+void raw_hid_receive(uint8_t *data, uint8_t length) {
+    // Validate input length
+    if (length < 2) {
+        print("LOKI: Invalid data length\n");
+        return;
+    }
+    
+    print("RECEIVED ON LOKI\n");
+    
+    uint8_t layer_num = data[0];
+    
+    // Bounds checking - this is critical!
+    if (layer_num >= COLOUR_MAP_SIZE) {
+        printf("LOKI: Invalid layer %d (max: %d)\n", layer_num, COLOUR_MAP_SIZE - 1);
+        return;
+    }
+    
+    hsv_t colour_struct = colour_map[layer_num];
+    printf("LOKI: Setting RGB to layer %d\n", layer_num);
+    
+    rgblight_sethsv(colour_struct.h, colour_struct.s, colour_struct.v);
+}
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[0] = LAYOUT_all( /* Base */
+    QK_GESC, KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
+    KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
+    KC_CAPS, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
+    KC_LSFT, KC_NUBS, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
+    KC_LCTL, KC_LGUI, KC_LALT,                        KC_SPC,                           MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+
+[1] = LAYOUT_all( /* FN */
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_VOLU, KC_VOLD, KC_MUTE,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+};
+
+
+
+
+
+
